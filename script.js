ff781e2831e7b8893df04850eb1df9bbc8a4aefb
unknown <kerimkara53@hotmail.com>
2021-12-14 23:40:37 +0300

    Switched from pure CSS to TailwindCSS

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-fetch('https://api.lanyard.rest/v1/users/349536885749579777')
-	.then((res) => res.json())
-	.then((lanyardData) => {
-		if (lanyardData && lanyardData.success && lanyardData.data && lanyardData.data.spotify) {
-			const spotify = lanyardData.data.spotify;
-			document.querySelector('.lanyard').innerHTML = `${spotify.artist} - ${spotify.song}`;
-		} else {
-            document.querySelector('.lanyard').innerHTML = `I'm not listening to anything right now`;
-        }
-	});
+fetch("https://api.lanyard.rest/v1/users/349536885749579777")
+  .then((res) => res.json())
+  .then((lanyardData) => {
+    if (
+      lanyardData &&
+      lanyardData.success &&
+      lanyardData.data &&
+      lanyardData.data.spotify
+    ) {
+      const spotify = lanyardData.data.spotify;
+      document.getElementById(
+        "lanyard"
+      ).innerHTML = `${spotify.artist} - ${spotify.song}`;
+      document
+        .getElementById("lanyard")
+        .setAttribute("title", `${spotify.artist} - ${spotify.song}`);
+    } else {
+      document.getElementById(
+        "lanyard"
+      ).innerHTML = `I'm not listening to anything right now`;
+    }
+  });
