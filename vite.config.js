7277d8b297b0acb8db86a33a9c96a164e092a9b9
ömer <42577096+omercup@users.noreply.github.com>
2021-12-26 00:25:18 +0300

    removed vite-svg-loader module

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite"
-import Vue from "@vitejs/plugin-vue"
-import SVGLoader from "vite-svg-loader"
-import WindiCSS from "vite-plugin-windicss"
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig({
-  base: "./",
-  plugins: [Vue(), SVGLoader(), WindiCSS()]
-})
+	base: './',
+	plugins: [Vue(), WindiCSS()],
+});
