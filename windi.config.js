96dc03a62382226f3c78cbdb574f7bb4b675aaa3
ömer <42577096+omercup@users.noreply.github.com>
2021-12-26 00:26:41 +0300

    renamed some color names

--- windi.config.js
+++ windi.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'windicss/helpers'
+import { defineConfig } from 'windicss/helpers';
 
 export default defineConfig({
-  theme: {
-    fontFamily: {
-      primary: ["Comfortaa"],
-      secondary: ["Gloria Hallelujah"]
-    },
-    colors: {
-      bg: "#242933",
-      card: "#2E3440",
-      icon: "#88C0D0",
-      primary: "#81a1c1",
-      secondary: "#D8DEE9"
-    }
-  }
-})
+	theme: {
+		fontFamily: {
+			primary: ['Gloria Hallelujah'],
+			secondary: ['Comfortaa'],
+		},
+		colors: {
+			background: '#242933',
+			card: '#2E3440',
+			icon: '#88C0D0',
+			primary: '#D8DEE9',
+			secondary: '#81A1C1',
+		},
+	},
+});
