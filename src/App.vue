884c3a654cbe62aaa8bfb7214afa13725b511791
ömer <42577096+omercup@users.noreply.github.com>
2021-12-26 00:28:49 +0300

    overall improvements

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,86 @@
 <template>
-  <section class="h-screen flex items-center justify-center bg-bg">
-    <figure class="p-8 md:p-16 w-5/6 md:w-auto bg-card rounded-4xl flex flex-col text-center gap-8">
-      <h1 class="font-secondary text-secondary text-6xl md:text-7xl font-bold">omercup</h1>
-      <p class="font-primary text-primary text-xl md:text-2xl flex items-start gap-4 md:w-lg justify-center">
-        <Disc class="text-icon animate-spin animate-duration-1600 flex-shrink-0 w-7 hidden md:block" v-if="lanyard" />
-        <span>{{ lanyard ? lanyard : "I'm not listening to anything right now" }}</span>
+  <section class="h-screen flex items-center justify-center bg-background select-none">
+    <figure class="p-8 md:p-12 w-5/6 md:w-auto bg-card rounded-4xl flex flex-col text-center gap-8">
+      <h1 class="font-primary text-primary text-6xl md:text-7xl font-bold">omercup</h1>
+      <p class="font-secondary text-secondary text-xl md:text-2xl flex items-start gap-4 md:w-lg justify-center">
+        <Icon
+          class="text-icon animate-spin animate-duration-1600 flex-shrink-0 w-7 hidden md:block"
+          icon="si-glyph:disc"
+          v-if="showIcon"
+          width="28"
+        />
+        <span>{{ songName ? songName : "Loading..." }}</span>
       </p>
 
       <div class="flex gap-8 justify-center">
-        <a href="https://discord.com/users/349536885749579777" target="_blank" rel="noreferrer noopener"><Discord class="transform transition duration-250 ease-out text-icon w-10 md:w-12 hover:(scale-125 text-secondary)" /></a>
-        <a href="https://github.com/omercup" target="_blank" rel="noreferrer noopener"><GitHub class="transform transition duration-250 ease-out text-icon w-10 md:w-12 hover:(scale-125 text-secondary)" /></a>
-        <a href="https://twitter.com/heisomercup" target="_blank" rel="noreferrer noopener"><Twitter class="transform transition duration-250 ease-out text-icon w-10 md:w-12 hover:(scale-125 text-secondary)" /></a>
+        <a
+          href="https://discord.com/users/349536885749579777"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <Icon
+            class="transform transition duration-250 ease-out text-icon w-10 md:w-12 hover:(scale-125 text-primary)"
+            icon="simple-icons:discord"
+            width="42"
+          />
+        </a>
+        <a 
+					href="https://github.com/omercup"
+					rel="noreferrer noopener"
+					target="_blank
+				>
+          <Icon
+            class="transform transition duration-250 ease-out text-icon w-10 md:w-12 hover:(scale-125 text-primary)"
+            icon="simple-icons:github"
+            width="42"
+          />
+        </a>
+        <a
+					href="https://steamcommunity.com/id/omercup/"
+					rel="noreferrer noopener"
+					target="_blank"
+				>
+          <Icon
+            class="transform transition duration-250 ease-out text-icon w-10 md:w-12 hover:(scale-125 text-primary)"
+            icon="simple-icons:steam"
+            width="42"
+          />
+        </a>
+        <a
+					href="https://twitter.com/heisomercup"
+					rel="noreferrer noopener"
+					target="_blank"
+				>
+          <Icon
+            class="transform transition duration-250 ease-out text-icon w-10 md:w-12 hover:(scale-125 text-primary)"
+            icon="simple-icons:twitter"
+            width="42"
+          />
+        </a>
       </div>
     </figure>
   </section>
 </template>
 
 <script setup>
-import { ref } from "vue"
-import { useLanyard } from "@leonardssh/use-lanyard"
+import { Icon } from '@iconify/vue';
+import { ref } from 'vue';
+import { useLanyard } from '@leonardssh/use-lanyard';
 
-import Disc from "./icons/disc.svg?component"
-import GitHub from "./icons/github.svg?component"
-import Discord from "./icons/discord.svg?component"
-import Twitter from "./icons/twitter.svg?component"
-
-const lanyard = ref(null)
+const showIcon = ref(false);
+const songName = ref('');
 
 useLanyard({
   socket: true,
-  userId: "349536885749579777",
+  userId: '349536885749579777',
   onPresenceUpdate(presence) {
-    if (presence.listening_to_spotify) lanyard.value = `${presence.spotify.artist} - ${presence.spotify.song}`; else lanyard.value = null
+    if (presence.listening_to_spotify) {
+      showIcon.value = true;
+      songName.value = `${presence.spotify.artist} - ${presence.spotify.song}`;
+    } else {
+      showIcon.value = false;
+      songName.value = `I'm not listening to anything right now`;
+    }
   }
-})
+});
 </script>
