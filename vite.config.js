f23f1a931dfadebd1423f6f100fbb901018a0a60
ömer <42577096+omercup@users.noreply.github.com>
2022-01-31 20:42:50 +0300

    delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import Vue from '@vitejs/plugin-vue';
-import WindiCSS from 'vite-plugin-windicss';
-
-export default defineConfig({
-	base: './',
-	plugins: [Vue(), WindiCSS()],
-});
